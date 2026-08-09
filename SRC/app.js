a242a5f0272e0167253e9517db7f21e94a770059
Monica <monica.lombardi1970@gmail.com>
2026-08-09 19:17:36 -0300

    app crada el 09-08-2026 donde se conecta al puerto sin problemase

---
+++ SRC/app.js
@@ -0,0 +1,11 @@
+import express from 'express';;
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('¡Hola mundo!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
